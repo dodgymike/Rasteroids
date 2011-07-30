39f4837279da4d0aa14038f7d010642bc1da0ee0
Mike Davis <mikedavis>
2011-07-31 00:41:09 +0200

    Added asteroids (kind thanks to http://galacticfool.com/asteroids-meteors-comets/)

--- bin/gosu_1.rb
+++ bin/gosu_1.rb
@@ -2,15 +2,20 @@
 
 require 'gosu'
 require 'lib/player'
+require 'lib/asteroid'
 
 class GameWindow < Gosu::Window
+  GAME_WINDOW_WIDTH = 640
+  GAME_WINDOW_HEIGHT = 480
+
   def initialize
-    super(640, 480, false)
+    super(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT, false)
     self.caption = "Gosu Tutorial Game"
 
     @background_image = Gosu::Image.new(self, "media/Space.bmp", true)
+    @asteroids = create_asteroids()
 
-    @player = Player.new(self)
+    @player = Player.new(self, Gosu::Image.new(self, "media/Starfighter.bmp", false))
     @player.warp(320, 240)
   end
 
@@ -29,13 +34,31 @@ class GameWindow < Gosu::Window
 
   def draw
     @player.draw
+    @asteroids.each do |asteroid|
+      asteroid.draw
+    end
+
     @background_image.draw(0, 0, 0);
+
   end
 
   def button_down(id)
     if id == Gosu::KbEscape
       close
     end
+  end
+
+  def create_asteroids
+    asteroids = (1 .. 3).collect do |index|
+      asteroid_image_name = "media/asteroid-#{index}.bmp"
+      puts "asteroid_image_name (#{asteroid_image_name})"
+      asteroid_image = Gosu::Image.new(self, asteroid_image_name, false)
+      asteroid = Asteroid.new(self, asteroid_image)
+      asteroid.warp(rand(GAME_WINDOW_WIDTH/2), rand(GAME_WINDOW_HEIGHT/2))
+      asteroid
+    end
+
+    asteroids
   end
 end
 
--- lib/asteroid.rb
+++ lib/asteroid.rb
@@ -1,4 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/game_entity.rb')
 
-class Player < GameEntity
+class Asteroid < GameEntity
 end
