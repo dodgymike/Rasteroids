5dbad1067a54ab0758e9e09bdc9358adcddfd8db
Mike Davis <mikedavis>
2011-07-30 23:32:18 +0200

    First working version from https://github.com/jlnr/gosu/wiki/Ruby-Tutorial

---
+++ bin/jukebox_server.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby -w
+# Jukebox Server
+
+require 'drb'
+
+Song = Struct.new 'Song', :title, :artist
+
+class Song
+  include DRbUndumped
+
+  def to_s
+    "#{title} by #{artist}"
+  end
+end
+
+class Jukebox
+  attr :songs
+
+  def initialize(songs)
+    @songs = songs
+  end
+
+  def play(index)
+    puts "playing #{@songs[index]}"
+  end
+end
+
+songs = [
+  Song.new("Amish Paradise", "Weird Al"),
+  Song.new("Eat it", "Weird Al")
+]
+
+DRb.start_service nil, Jukebox.new(songs)
+puts DRb.uri
+
+DRb.thread.join
